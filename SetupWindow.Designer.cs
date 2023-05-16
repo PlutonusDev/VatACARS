962d6d4a432cb1b7434d4baab8e1a1306f0aae2b
Scorcher Plays <scorcherplaysyt@gmail.com>
2023-05-16 21:57:40 +0800

    edit ui

--- SetupWindow.Designer.cs
+++ SetupWindow.Designer.cs
@@ -31,15 +31,21 @@ namespace vStripsPlugin
         {
             this.cancelButton = new vatsys.GenericButton();
             this.b_restartPlugin = new vatsys.GenericButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.Font = new System.Drawing.Font("Terminus (TTF)", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.cancelButton.Location = new System.Drawing.Point(147, 161);
+            this.cancelButton.Location = new System.Drawing.Point(209, 114);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(90, 28);
+            this.cancelButton.Size = new System.Drawing.Size(120, 28);
             this.cancelButton.SubFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cancelButton.SubText = "";
             this.cancelButton.TabIndex = 1;
@@ -50,38 +56,107 @@ namespace vStripsPlugin
             // 
             this.b_restartPlugin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.b_restartPlugin.Font = new System.Drawing.Font("Terminus (TTF)", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.b_restartPlugin.Location = new System.Drawing.Point(12, 161);
+            this.b_restartPlugin.Location = new System.Drawing.Point(12, 114);
             this.b_restartPlugin.Name = "b_restartPlugin";
-            this.b_restartPlugin.Size = new System.Drawing.Size(90, 28);
+            this.b_restartPlugin.Size = new System.Drawing.Size(120, 28);
             this.b_restartPlugin.SubFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.b_restartPlugin.SubText = "";
             this.b_restartPlugin.TabIndex = 9;
             this.b_restartPlugin.Text = "Save";
             this.b_restartPlugin.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Terminus (TTF)", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 26);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ATC Callsign:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Terminus (TTF)", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.textBox1.Location = new System.Drawing.Point(163, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 26);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Terminus (TTF)", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 26);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ACARS Server:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Terminus (TTF)", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(12, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 26);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ACARS Logon:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Terminus (TTF)", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.textBox2.Location = new System.Drawing.Point(164, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Terminus (TTF)", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.textBox3.Location = new System.Drawing.Point(163, 41);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(166, 26);
+            this.textBox3.TabIndex = 15;
+            // 
             // SetupWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(248, 202);
+            this.ClientSize = new System.Drawing.Size(341, 155);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.b_restartPlugin);
             this.Controls.Add(this.cancelButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.HasMinimizeButton = false;
             this.HasSendBackButton = false;
+            this.MaximumSize = new System.Drawing.Size(345, 183);
             this.MiddleClickClose = false;
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(345, 183);
             this.Name = "SetupWindow";
             this.Resizeable = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "vatACARS Setup";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private vatsys.GenericButton cancelButton;
         private vatsys.GenericButton b_restartPlugin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
